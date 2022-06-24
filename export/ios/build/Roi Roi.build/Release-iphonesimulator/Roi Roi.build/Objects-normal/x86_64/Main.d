7bc5e0c61eb306f59f7b7acb8ae7a5c5c2d77a3f
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2022-06-24 04:53:29 -0700

    Delete export directory

--- export/ios/build/Roi Roi.build/Release-iphonesimulator/Roi Roi.build/Objects-normal/x86_64/Main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/towster/Desktop/Coding\ Projects/HaxeFlixel/Roi\ Roi/export/ios/Roi\ Roi/Roi\ Roi-Prefix.pch \
-  /Users/towster/Desktop/Coding\ Projects/HaxeFlixel/Roi\ Roi/export/ios/Roi\ Roi/Classes/Main.mm
--- export/ios/build/SharedPrecompiledHeaders/SharedPrecompiledHeaders/4459035062071968965/Roi Roi-Prefix.pch.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/towster/Desktop/Coding\ Projects/HaxeFlixel/Roi\ Roi/export/ios/Roi\ Roi/Roi\ Roi-Prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.2.sdk/usr/include/Availability.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.2.sdk/usr/include/AvailabilityVersions.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.2.sdk/usr/include/AvailabilityInternal.h
